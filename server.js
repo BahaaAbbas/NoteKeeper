f2a61bbccad4ccdb83394e9fef5cb89bf6a0d8d1
Bahaa Abbas <bahaaisl566@gmail.com>
2025-08-18 15:01:15 +0300

    simple MVC

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "@dotenvx/dotenvx";
 import connectMDB from "./db.js";
+import notesRouter from "./routes/notesRoute.js";
 
 dotenv.config();
 
@@ -14,6 +15,18 @@ app.use(express.static("public"));
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
+
+app.use("/notes", notesRouter);
+
+app.get("/", (req, res) => {
+  res.redirect("/notes");
+});
+
+app.use((req, res, next) => {
+  res
+    .status(404)
+    .render("notfound", { title: "Page Not Found", path: req.originalUrl });
+});
 
 const PORT = process.env.PORT || 8000;
 
